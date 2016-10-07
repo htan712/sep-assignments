3b4a91a29bcf7a4f8f8f0b7769ddb6f72b8f8dcf
htan712 <hock.tan712@gmail.com>
2016-10-07 14:05:06 -0700

    my own push and pop method

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,12 +7,24 @@ class MyStack
   end
 
   def push(item)
-    @stack.unshift(item)
+    i = @stack.size 
+    until i < 0 do
+      @stack[i] = @stack[i-1]
+      if i == 0 
+        @stack[i] = item
+      end
+      i-=1
+    end
     self.top = @stack[0]
   end
 
   def pop
-    item = @stack.shift
+    item = @stack[0]
+    i = 0
+    @stack.each do |x|
+      @stack[i] = @stack[i+1]
+      i+=1
+    end
     self.top = @stack[0]
     item
   end
